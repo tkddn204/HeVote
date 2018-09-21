79456c036756d1a61181428724cec82151a89a80
SsangWoo <tkddn204@gmail.com>
2018-09-21 15:33:21 +0900

    영어로 수정

--- app/ethereum/migrations/2_deploy_contracts.js
+++ app/ethereum/migrations/2_deploy_contracts.js
@@ -11,8 +11,10 @@ module.exports = (deployer, network, accounts) =>
         const deployedElectionFactory = await ElectionFactory.deployed();
 
         await deployedElectionFactory.makeNewElection(
-            '6.13 대전 지방선거',
-            '이번 선거에서 선출된 공직자의 임기는 4년(2018. 7. 1.~2022. 6. 30.)이며, 재보궐선거의 경우 전임자의 잔여임기(~2020. 5. 29.)까지 업무를 수행하게 된다.',
+            // '대전 지방선거'
+            'Daejeon local election',
+            // '이번 선거에서 선출된 공직자의 임기는 4년(2018. 7. 1.~2022. 6. 30.)이며, 재보궐선거의 경우 전임자의 잔여임기(~2020. 5. 29.)까지 업무를 수행하게 된다.',
+            'The Daejeon local election is an election of the Republic of Korea to elect local councilors and heads of local autonomous bodies that make up the local council.',
             accounts[1],
             timeUtil.dateStringToTimestamp('09/10/2018 06:00:00'),
             timeUtil.dateStringToTimestamp('10/10/2018 18:00:00'),
@@ -23,12 +25,16 @@ module.exports = (deployer, network, accounts) =>
 
         // 지방선거 투표에 후보자 추가
         const deployedRegionElection = await Election.at(deployedPublicElections[0]);
-        const regionCandidateList = ['홍길동', '김영희', '박철수', '고길동'];
+        const regionCandidateList = ['John Doe','Jane Roe','John Smith','Jane Smith'] //['홍길동', '김영희', '박철수', '고길동'];
         const regionCandidateCommitment = [
-            '시민을 위해 노력하겠습니다!',
-            '올바른 정치를 하겠습니다!',
-            '밝은 사회를 위해 전진하겠습니다!',
-            '경제 발전을 위해 행동하겠습니다!'
+            'I will do my best for the citizens!',
+            'I will do the right politics!',
+            'I will move forward for a bright society!',
+            'I will act for economic development!'
+            // '시민을 위해 노력하겠습니다!',
+            // '올바른 정치를 하겠습니다!',
+            // '밝은 사회를 위해 전진하겠습니다!',
+            // '경제 발전을 위해 행동하겠습니다!'
         ];
         for (let i = 0; i < regionCandidateList.length; i++) {
             await deployedRegionElection.addCandidate(
@@ -52,15 +58,6 @@ module.exports = (deployer, network, accounts) =>
                     fs.close(fd, () => console.log(JSON.stringify(jsonObj)));
                 });
         });
-
-
-        if(!fs.existsSync('./data')) {
-            fs.mkdirSync('./data');
-            fs.mkdirSync('./data/candidate');
-            fs.mkdirSync('./data/publicKey');
-            fs.mkdirSync('./data/result');
-            fs.mkdirSync('./data/secretKey');
-        }
 
         // hec으로 공개키를 저장합니다.
         await hec.createKeys(deployedPublicElections[0], 10007, 7, 'data', async () => {
