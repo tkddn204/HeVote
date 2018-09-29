8307fe33218eefef8f9dbe3cb633d4717c8fcdb2
SsangWoo <tkddn204@gmail.com>
2018-09-29 18:55:35 +0900

    detail 버튼 수정

--- public/javascripts/election.detail.js
+++ public/javascripts/election.detail.js
@@ -1,6 +1,23 @@
 $(document).ready(() => {
 
-    $("button[class=btn-primary]").click((event) => {
+    $(".btn[type=submit]").click((event) => {
+        const $this = $(event.target);
+        const loadingText = "투표 중입니다...";
+
+        const candidate = $('input[name="candidate"]:checked').val();
+        if (!candidate) {
+            alert("후보를 선택해주세요!");
+            return;
+        } else {
+            $("form").submit();
+        }
+        if ($this.html() !== loadingText) {
+            $this.html(loadingText);
+            $this.attr('disabled', true);
+        }
+    });
+
+    $("#participateVote").click((event) => {
         const $this = $(event.target);
         const loadingText = "잠시만 기다려주세요...";
 
@@ -8,6 +25,7 @@ $(document).ready(() => {
             $this.html(loadingText);
             $this.attr('disabled', true);
         }
+        window.location.replace($this.attr('data'));
     });
 
     $("button[id^='commitmentBtn']").click((event) => {
