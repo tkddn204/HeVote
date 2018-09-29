972d129bfa380bbba878a2a72bd26755bd214c73
SsangWoo <tkddn204@gmail.com>
2018-09-29 19:06:35 +0900

    election.detail.js 수정

--- public/javascripts/election.detail.js
+++ public/javascripts/election.detail.js
@@ -1,30 +1,23 @@
 $(document).ready(() => {
 
-    $(".btn[type=submit]").click((event) => {
+    const changeTextWhileLoading = (event, loadingText) => {
         const $this = $(event.target);
-        const loadingText = "투표 중입니다...";
 
-        const candidate = $('input[name="candidate"]:checked').val();
-        if (!candidate) {
-            alert("후보를 선택해주세요!");
-            return;
-        } else {
-            $("form").submit();
-        }
         if ($this.html() !== loadingText) {
             $this.html(loadingText);
             $this.attr('disabled', true);
         }
+    };
+
+    $(".btn[type='submit']").click((event) => {
+        const loadingText = "변경 중입니다...";
+        changeTextWhileLoading(event, loadingText);
     });
 
     $("#participateVote").click((event) => {
-        const $this = $(event.target);
         const loadingText = "잠시만 기다려주세요...";
+        changeTextWhileLoading(event, loadingText);
 
-        if ($this.html() !== loadingText) {
-            $this.html(loadingText);
-            $this.attr('disabled', true);
-        }
         window.location.replace($this.attr('data'));
     });
 
