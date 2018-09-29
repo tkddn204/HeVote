8d5ecf6a572056edff2753293c6175dcf4065416
Ubuntu <ubuntu@ip-172-31-29-249.ap-northeast-2.compute.internal>
2018-09-29 16:13:28 +0000

    remake 수정

---
+++ scripts/remake.sh
@@ -0,0 +1,14 @@
+rm -rf app/ethereum/build
+rm -rf backup/data/*
+mkdir backup
+mv data backup/data
+mkdir data
+mkdir data/candidate
+mkdir data/publicKey
+mkdir data/secretKey
+mkdir data/result
+truffle migration --reset
+node scripts/ipfs.region.js
+node scripts/mongo.account.update.js
+
+
