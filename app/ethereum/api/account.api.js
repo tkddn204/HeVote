22217a1de5cba3376ce90f64e06ae8d8df16e37c
SsangWoo <tkddn204@gmail.com>
2018-10-29 16:43:39 +0900

    계정 생성부분 변경

--- app/ethereum/api/account.api.js
+++ app/ethereum/api/account.api.js
@@ -1,7 +1,16 @@
 const web3 = require('../web3');
+const contractInformation = require('../../../config/contract-address.json');
 
-const makeNewAccount = async (password) =>
-    await web3.eth.accounts.create(password);
+const makeNewAccount = async (password) => {
+    const account = await web3.eth.accounts.create(password);
+    await web3.eth.sendTransaction({
+        from: contractInformation['admin_address'],
+        to: account.address,
+        value: web3.utils.toWei('0.1', 'ether'),
+        gasLimit: 21000
+    });
+    return account
+};
 
 module.exports = {
     makeNewAccount
