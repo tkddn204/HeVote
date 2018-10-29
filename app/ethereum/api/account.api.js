6d8144b6953b783b9303e5235aa22a88cd3f0b07
SsangWoo <tkddn204@gmail.com>
2018-10-29 17:25:50 +0900

    accounts 수정(ganache)

--- app/ethereum/api/account.api.js
+++ app/ethereum/api/account.api.js
@@ -2,10 +2,11 @@ const web3 = require('../web3');
 const contractInformation = require('../../../config/contract-address.json');
 
 const makeNewAccount = async (password) => {
-    const account = await web3.eth.accounts.create(password);
+    const account = await web3.eth.personal.newAccount(password);
+    await web3.eth.personal.unlockAccount(account, password, 100000);
     await web3.eth.sendTransaction({
         from: contractInformation['admin_address'],
-        to: account.address,
+        to: account,
         value: web3.utils.toWei('0.1', 'ether'),
         gasLimit: 21000
     });
