5de908919da0429f6b63c27a36fef20d42fdfeba
SsangWoo <tkddn204@gmail.com>
2018-10-29 17:47:45 +0900

    mongo.account.update 수정2

--- scripts/mongo.account.update.js
+++ scripts/mongo.account.update.js
@@ -5,31 +5,33 @@ const config = require('../config');
 
 
 const mongoAccountUpdate = (electionOwner, electionAddress, finiteElection) =>
-    new Promise((resolve, reject) => mongoose.connect(config.db, {useNewUrlParser: true})
-        .then(() => {
-            console.log("mongoDB connected.");
-            Account.update(
-                {"etherAccount": electionOwner},
-                {
-                    "$push": {
-                        "deployedElections": {
-                            address: electionAddress,
-                            finite: finiteElection
-                        }
+    new Promise((resolve, reject) => {
+        mongoose.connect(config.db, {useNewUrlParser: true})
+            .then(() => {
+                console.log("mongoDB connected.");
+            })
+            .catch(err => {
+                console.error(err.message);
+                reject(err);
+            });
+        Account.update(
+            {"etherAccount": electionOwner},
+            {
+                "$push": {
+                    "deployedElections": {
+                        address: electionAddress,
+                        finite: finiteElection
                     }
-                },
-                (err) => {
-                    if (err) return reject(err);
-                    else console.log("Saved to MongoDB!");
-                    mongoose.connection.close();
-                    resolve();
                 }
-            );
-        })
-        .catch(err => {
-            console.error(err.message);
-            reject(err);
-        })
+            },
+            (err) => {
+                if (err) return reject(err);
+                else console.log("Saved to MongoDB!");
+                mongoose.connection.close();
+                resolve(true);
+            }
+        );
+    }
 );
 
 module.exports = mongoAccountUpdate;
